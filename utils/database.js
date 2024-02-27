95f4b145e938c37798be83f92d4d2a89cc04d2ef
Atif Razzaq <razzaq6atif@gmail.com>
2024-02-27 16:10:52 +0500

    Initial Commits

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already Connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "shared_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB Connection Error", error);
+  }
+};
